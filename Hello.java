c058f59eeceaacb9ef7f09fac81bbd3145e6a6e8
Anjali-Sharmarpr <anjalirpr01@gmail.com>
2023-02-15 22:03:24 +0530

    Adding basic Programs

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+      float num=3.5f;
+      byte by=12;
+      int number=0xF;
+      System.out.println("Hello World");
+      System.out.println(by);
+      System.out.println(num);
+      System.out.println(number);
+    }
+};
---
+++ userinput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class userinput {
+    public static void main(String [] args){
+       Scanner input = new Scanner(System.in);
+       int data=input.nextInt();
+       System.out.println(data);
+    }
+}
