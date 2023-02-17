d313087f42e931f150c1af7bc8bd9ab65271eb58
Anjali-Sharmarpr <anjalirpr01@gmail.com>
2023-02-18 01:32:20 +0530

    IDE programs

---
+++ usingString.java
@@ -0,0 +1,17 @@
+package PracticeBasic;
+
+public class usingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name =new String("Anjali Sharma");
+		System.out.println(name);
+		
+		//concatenate
+		System.out.println("Hello "+ name);
+		String word = "No_Object";
+		System.out.println("String with " + word);
+		
+	}
+
+}
